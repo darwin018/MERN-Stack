d8910fc40d664845714e1b9d7d203b4fa345252c
darwin018 <darwin17012005@gmail.com>
2025-02-14 12:16:31 +0530

    day 6 FN commit

---
+++ javascript/operators.js
@@ -0,0 +1,20 @@
+// // logical operator
+// // logical AND
+// console.log(5 && 10)
+// console.log(true && 10)
+// console.log(false && 10)
+// console.log(false && true)
+// console.log(true && false)
+// // logical OR
+// console.log(5 || 10)
+// console.log(true || 10)
+// console.log(false || 10)
+// console.log(false || true)
+// console.log(true || false)
+
+
+// Relational operator
+console.log(5=='5')
+console.log(5==='5')
+console.log()
+
---
+++ javascript/print.js
@@ -0,0 +1,46 @@
+// primitive data types
+let a=20
+var b="abc"
+const c=false
+// console.log(a,b,c)
+
+
+// secondary data types
+
+arr = [1,6,"abc",true]
+// console.log(arr[3])
+// console.log(arr)
+
+// objects
+// 1st type of object declaration
+
+object1 = {
+    "firstName" : "St.Josep's",
+    "secondName" : "Institute of Technology",
+    "dept" : ["CSE","IT","ECE","ADS"],
+    "training" : "MERN"
+}
+object1.dept[4] = "MECH"
+// console.log(object1.dept)
+// console.log(object1["dept"])
+
+// 2nd type of object declaration
+object2 = {}
+object2["firstName"] = "St.Joseph's"
+// console.log(object2)
+
+// 3rd type of object declaration
+object3 = new Object()
+// console.log(object3)
+object3.training = "MERN"
+object3.dept = ["CSE","IT"]
+// console.log(object3)
+
+// Set declaration
+set = new Set(["hello"])
+// console.log(set)
+set.add("SJIT")
+// console.log(set)
+// console.log(typeof(set))
+
+
