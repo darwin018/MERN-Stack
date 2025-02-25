63c010335dffbfbb38063dfea31de881a863dae2
darwin018 <darwin17012005@gmail.com>
2025-02-25 13:18:43 +0530

    day 13 FN commit

--- sjitBackend/index.js
+++ sjitBackend/index.js
@@ -1,11 +1,44 @@
 const express = require("express");
+const mdb = require('mongoose');
+const dotenv = require('dotenv')
+const Signup = require('./models/signupSchema')
 
 const app = express();
+app.use(express.json())
+const PORT = 3001;
+dotenv.config()
 
-app.get("/",(req,res) => {
-    res.send("<h1>Welcome to backend server..</h1>")
+console.log("mongodb+srv://darwin018:Darwin2005@sjitmern2025.0ftin.mongodb.net/")
+
+mdb.connect(process.env.MONGODB_URL).then(() => {
+    console.log("Mongo DB connection successfull")
+}).catch((err) => {
+    console.log("Check your connection string",err)
 })
 
-const PORT = 3001;
+app.get("/static",(req,res) => {
+    res.sendFile("D:\\MERN_Stack\\HTML_CSS\\index.html")
+})
+
+app.post("/signup",(req,res) =>{
+    try{
+        const {firstName,lastName,email,password,phoneNumber} = req.body
+        const newSignup = new Signup({
+            firstName:"Darwin",
+            lastName:"Ramakrishnan",
+            email:"darwin@sjit.ac.in",
+            password:"sjit",
+            phoneNumber:9344816453
+
+        })
+        newSignup.save()
+        console.log("Signup successful")
+        res.status(201).json({message:"signup successfull",isSignup : true})
+    }
+    catch(error){
+        console.log(error)
+        res.status(201).json({message:"signup unsuccessfull",isSignup : false})
+    }
+})
 
 app.listen(PORT, () => console.log("Server Started successfully."));
